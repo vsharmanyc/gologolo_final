9a6e13c457364161bdbe91acc6d0e0e3b45f6417
vsharmanyc <vsharma.nyc@gmail.com>
2020-04-09 00:47:14 -0400

    Updated the GraphQL Logo API (logoType, addLogo and updateLogo mutations)

--- server/graphql/logoSchemas.js
+++ server/graphql/logoSchemas.js
@@ -25,6 +25,24 @@ var logoType = new GraphQLObjectType({
             fontSize: {
                 type: GraphQLInt
             },
+            backgroundColor: {
+                type: GraphQLString
+            },
+            borderColor: {
+                type: GraphQLString
+            },
+            borderRadius: {
+                type: GraphQLInt
+            },
+            borderWidth: {
+                type: GraphQLInt
+            },
+            padding: {
+                type: GraphQLInt
+            },
+            margin: {
+                type: GraphQLInt
+            },
             lastUpdate: {
                 type: GraphQLDate
             }
@@ -81,6 +99,24 @@ var mutation = new GraphQLObjectType({
                     },
                     fontSize: {
                         type: new GraphQLNonNull(GraphQLInt)
+                    },
+                    backgroundColor: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    borderColor: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    borderRadius: {
+                        type: new GraphQLNonNull(GraphQLInt)
+                    },
+                    borderWidth: {
+                        type: new GraphQLNonNull(GraphQLInt)
+                    },
+                    padding: {
+                        type: new GraphQLNonNull(GraphQLInt)
+                    },
+                    margin: {
+                        type: new GraphQLNonNull(GraphQLInt)
                     }
                 },
                 resolve: function (root, params) {
@@ -107,10 +143,39 @@ var mutation = new GraphQLObjectType({
                     },
                     fontSize: {
                         type: new GraphQLNonNull(GraphQLInt)
+                    },
+                    backgroundColor: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    borderColor: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    borderRadius: {
+                        type: new GraphQLNonNull(GraphQLInt)
+                    },
+                    borderWidth: {
+                        type: new GraphQLNonNull(GraphQLInt)
+                    },
+                    padding: {
+                        type: new GraphQLNonNull(GraphQLInt)
+                    },
+                    margin: {
+                        type: new GraphQLNonNull(GraphQLInt)
                     }
                 },
                 resolve(root, params) {
-                    return LogoModel.findByIdAndUpdate(params.id, { text: params.text, color: params.color, fontSize: params.fontSize, lastUpdate: new Date() }, function (err) {
+                    return LogoModel.findByIdAndUpdate(params.id, { 
+                        text: params.text, 
+                        color: params.color, 
+                        fontSize: params.fontSize,
+                        backgroundColor: params.backgroundColor,
+                        borderColor: params.borderColor,
+                        borderRadius: params.borderRadius,
+                        borderWidth: params.borderWidth,
+                        padding: params.padding,
+                        margin: params.margin, 
+                        lastUpdate: new Date() 
+                    }, function (err) {
                         if (err) return next(err);
                     });
                 }
