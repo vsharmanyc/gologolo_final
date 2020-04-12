4c5800974258ea65445ec97a6e7d5d8e62db4983
vsharmanyc <vsharma.nyc@gmail.com>
2020-04-12 12:31:46 -0400

    home screen ui fixed

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -20,29 +20,30 @@ class HomeScreen extends Component {
         return (
             <Query pollInterval={500} query={GET_LOGOS}>
                 {({ loading, error, data }) => {
-                    if(data.logos !== undefined)
-                        data.logos.sort((a,b) => new Date(b.lastUpdate) - new Date(a.lastUpdate));
+                    if (data.logos !== undefined)
+                        data.logos.sort((a, b) => new Date(b.lastUpdate) - new Date(a.lastUpdate));
                     if (loading) return 'Loading...';
                     if (error) return `Error! ${error.message}`;
 
                     return (
-                        <div className="container row">
-                            <div className="col s4">
-                                <h3>Recent Work</h3>
-                                {data.logos.map((logo, index) => (
-                                    <div key={index} className='home_logo_link'
-                                        style={{ cursor: "pointer" }}>
-                                        <Link to={`/view/${logo._id}`}>{logo.text.replace(/\s/g, '\u00A0')}</Link>
-                                    </div>
-                                ))}
-                            </div>
-                            <div className="col s8">
-                                <div id="home_banner_container">
-                                    @todo<br />
-                                    List Maker
+                        <div className="container" id="home_screen_container">
+                            <div className="container row">
+                                <div className="col s4">
+                                    <h3 id="recent_work_heading">Recent Work</h3>
+                                    {data.logos.map((logo, index) => (
+                                        <div key={index} className='home_logo_link'
+                                            style={{ cursor: "pointer" }}>
+                                            <Link to={`/view/${logo._id}`} class="home_work_link">{logo.text.replace(/\s/g, '\u00A0')}</Link>
+                                        </div>
+                                    ))}
                                 </div>
-                                <div>
-                                    <Link id="add_logo_button" to="/create">Add Logo</Link>
+                                <div className="col s8">
+                                    <div id="home_banner_container">
+                                        GoLogoLo
+                                    </div>
+                                    <div>
+                                        <Link id="add_logo_button" to="/create"><button id="new_logo_button">Create New Logo</button></Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
